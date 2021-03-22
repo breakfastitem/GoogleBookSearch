edbabf065bdc895c366dfed3025eec2b07c4d37b
breakfastitem <andrew.ehrman@gmail.com>
2021-03-22 11:39:22 -0400

    Added title card template

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Saved from './pages/Saved/saved';
 import Search from './pages/Search/search';
 import Header from './components/Header/header';
+import TitleCard from './components/TitleCard/titleCard';
 
 function App() {
   return (
-    
+
     <div className="container">
       <BrowserRouter>
         <Header />
+        <TitleCard/>
         <Switch>
           <Route path="/" exact component={() => <Saved />} />
           <Route path="/saved" exact component={() => <Saved />} />
---
+++ client/src/components/TitleCard/titleCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+function TitleCard() {
+    return (
+        <div className="row">
+            <div className="col-12 text-center">
+                <h1>(React) Google Book Search </h1>
+                <h2>Search For and Save Books of Interest</h2>
+            </div>
+        </div>);
+}
+
+
+export default TitleCard;
