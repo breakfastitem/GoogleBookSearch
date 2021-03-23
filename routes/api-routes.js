141baa4efa8770f924a03aee7d8d2e54238e4baf
breakfastitem <andrew.ehrman@gmail.com>
2021-03-22 20:00:44 -0400

    Added mongodb routes for adding,removing getting local books

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,32 @@
-module.exports = function(app,db){
+module.exports = function (app, db) {
 
-    app.get("/api/books",()=>{
+    //gets saved books from api
+    app.get("/api/books", (req, res) => {
+        db.Book.find({}, (err, data) => {
+            if (err) throw err;
 
+            res.json(data);
+        });
+    });
+
+    //Add book to api
+    app.post("/api/books", ({ body }, res) => {
+        let bookObj = new db.Book(body);
+
+        db.Book.create(bookObj, (err, book) => {
+            if (err) throw err;
+
+            res.json(book);
+        });
+    });
+
+    //Remove book by id
+    app.delete("/api/books/:id", (req, res) => {
+        db.Book.findOneAndRemove({ _id: req.params.id }, (err, data) => {
+            if (err) throw err;
+
+            res.json(data);
+        });
     });
 
 }
