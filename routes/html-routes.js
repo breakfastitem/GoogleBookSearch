673ad12e81377f62c357244cf89378a983c4ec8f
breakfastitem <andrew.ehrman@gmail.com>
2021-03-24 00:19:20 -0400

    fixed html route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,5 @@
-const path = require("path");
-
 module.exports = function (app) {
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "/client/build/index.html"));
+        res.sendFile("./client/build/index.html");
     })
 }
